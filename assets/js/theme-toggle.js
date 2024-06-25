d791f36dfacc46dde77c49b3a4c4f42019d5c7dd
smanotasa <santiago.manotasarroyave@bse.eu>
2024-06-25 21:42:05 +0200

    Some tests

---
+++ assets/js/theme-toggle.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+    const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+  
+    if (currentTheme) {
+      document.documentElement.setAttribute('data-theme', currentTheme);
+  
+      if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        document.body.classList.add('dark-mode');
+      }
+    }
+  
+    function switchTheme(e) {
+      if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        document.body.classList.add('dark-mode');
+      } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        document.body.classList.remove('dark-mode');
+      }    
+    }
+  
+    toggleSwitch.addEventListener('change', switchTheme, false);
+  });
+  
